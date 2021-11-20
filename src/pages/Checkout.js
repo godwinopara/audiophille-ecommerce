4a9665c1e9347cade18db8ae9ba01e8acd4ea87f
godwin opara <godwinopara62@yahoo.com>
2021-11-20 14:29:09 +0400

    install and setup tailwindcss

---
+++ src/pages/Checkout.js
@@ -0,0 +1,69 @@
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <div>
+      Home Headphones Speakers Earphones
+      <br />
+      {/* <!-- Cart Modal --> */}
+      Cart(3) Remove All
+      <br />
+      XX99 MK II x1 $2,999
+      <br />
+      XX59 x2 $899
+      <br />
+      YX1 x1 $599
+      <br />
+      Total $5,396
+      <br />
+      Checkout
+      {/* <!-- Cart modal end --> */}
+      <br />
+      Go back
+      <br />
+      Checkout
+      <br />
+      Billing details Name Email address Phone number
+      <br />
+      Shipping info Address ZIP code City Country
+      <br />
+      Payment details Payment method e-Money Cash on delivery e-Money number e-money PIN
+      <br />
+      The ‘Cash on Delivery’ option enables you to pay in cash when our delivery courier arrives at
+      your residence. Just make sure your address is correct so that your order will not be
+      cancelled.
+      <br />
+      Summary XX99 MK II x1 $2,999
+      <br />
+      XX59 x2 $899
+      <br />
+      YX1 x1 $599
+      <br />
+      Total $5,396 Shipping $10 VAT $120
+      <br />
+      Grand total $5,526
+      <br />
+      Continue & pay
+      <br />
+      {/* <!-- Success Modal --> */}
+      Thank you for your order You will receive an email confirmation shortly.
+      <br />
+      XX99 MK II x 1 $2,999 and 2 other item(s)
+      <br />
+      Grand total $5,526
+      <br />
+      Back to home
+      {/* <!-- End success modal --> */}
+      <br />
+      Home Headphones Speakers Earphones
+      <br />
+      Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music
+      lovers and sound specialists who are devoted to helping you get the most out of personal
+      audio. Come and visit our demo facility - we’re open 7 days a week.
+      <br />
+      Copyright 2021. All Rights Reserved
+    </div>
+  );
+};
+
+export default Checkout;
---
+++ src/pages/Home.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <br />
+      Home Headphones Speakers Earphones
+      <br />
+      New product XX99 Mark II Headphones Experience natural, lifelike audio and exceptional build
+      quality made for the passionate music enthusiast.
+      <br />
+      See product
+      <br />
+      Headphones Shop
+      <br />
+      Speakers Shop
+      <br />
+      Earphones Shop
+      <br />
+      ZX9 speaker Upgrade to premium speakers that are phenomenally built to deliver truly
+      remarkable sound. See product
+      <br />
+      ZX7 speaker See product
+      <br />
+      YX1 earphones See product
+      <div>
+        Bringing you the best audio gear Located at the heart of New York City, Audiophile is the
+        premier store for high end headphones, earphones, speakers, and audio accessories. We have a
+        large showroom and luxury demonstration rooms available for you to browse and experience a
+        wide range of our products. Stop by our store to meet some of the fantastic people who make
+        Audiophile the best place to buy your portable audio equipment.
+      </div>
+      <div>
+        Home
+        <br />
+        Headphones
+        <br />
+        Speakers
+        <br />
+        Earphones
+      </div>
+      Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music
+      lovers and sound specialists who are devoted to helping you get the most out of personal
+      audio. Come and visit our demo facility - we’re open 7 days a week.
+      <br />
+      Copyright 2021. All Rights Reserved
+    </div>
+  );
+};
+
+export default Home;
